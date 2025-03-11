9fc4dc1921ea6d1dc24fab26f46e123ae73a05c0
RodrigoC28 <rodrigoscosta28@gmail.com>
2025-03-11 14:34:41 +0000

    Dockerfile adicionado

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require("express");
 let app = express();
 
-app.listen(80, () =>{
-    console.log("PING! Recebi um pedido HTTP!");
-});
-
 app.get("/", (req, res) => {
     res.send("Bem-Vindo ao servidor Web!");
 });
